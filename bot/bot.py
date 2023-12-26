3086c5150e36b02d522074800ed75b5b68bbda04
Vladimir Dimitrov <v.dimitrov@g.nsu.ru>
2023-12-26 23:55:16 +0300

    add constant messages: /start and /help

--- bot/bot.py
+++ bot/bot.py
@@ -2,7 +2,7 @@ import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
+import message_text
 
 
 logging.basicConfig(
@@ -17,12 +17,18 @@ if not TELEGRAM_BOT_TOKEN:
     exit("Please specify telegram-bot token environment variable")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот, поговорим?")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message_text.GREETINGS)
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message_text.HELP)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    help_handler = CommandHandler('help', help)
+    application.add_handler(help_handler)
     
     application.run_polling()
